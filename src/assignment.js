38ce46974444a80f1b13c17dc550739276971f3b
Victoria Amarachi Philip <victoriamarachi450@gmail.com>
2022-07-11 21:21:22 +0000

    Assignment 1& 2

--- src/assignment.js
+++ src/assignment.js
@@ -14,9 +14,12 @@ const assignment = {};
  * @returns number the sum of the numbers
  */
 function sumOfNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+    let sum =0
+  for(let d of arrayOfNumbers)
+    {sum= sum + d}
+  return sum;
 }
-// assignment.sumOfNumbers = sumOfNumbers;
+assignment.sumOfNumbers = sumOfNumbers;
 
 /**
  * Challenge - 2
@@ -28,9 +31,15 @@ function sumOfNumbers(arrayOfNumbers) {
  * @returns number the count of even numbers
  */
 function countEvenNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+    let count =0
+  for(let d of arrayOfNumbers) {
+    if (d % 2 === 0)  {
+      count= count + 1
+    }
+  }
+   return count;
 }
-// assignment.countEvenNumbers = countEvenNumbers;
+assignment.countEvenNumbers = countEvenNumbers;
 
 /**
  * Challenge - 3
